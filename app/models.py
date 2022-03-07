39f799d60430c75a43a3db16c526ad69c030a295
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-07 11:43:04 +0300

    Add a script to check database is created, and if not, create it

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+# from datetime import datetime
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,10 +10,10 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(255))
     pitches = db.relationship('Pitch')
 
-class Pitch(db.model):
+class Pitch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(15000))
-    date = (db.Column(timezone=True), default=func.now())
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.column(db.Integer, db.ForeignKey('user.id'))
 
 
