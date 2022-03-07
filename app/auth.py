9d3c662aa8c6efb93631aad5a0404143f1215860
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-07 07:54:58 +0300

    Add the methods to post data for our sign up form

--- app/auth.py
+++ app/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
     return render_template('login.html', boolean=True)
 
@@ -10,6 +10,12 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
     return render_template('sign_up.html')
