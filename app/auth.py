6d2afef36d997a26e82872bca888bd6396894b8c
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-07 12:17:26 +0300

    Add password hashing

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,6 @@
 from flask import Blueprint,render_template,request,flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth',__name__)
 
@@ -28,5 +30,6 @@ def sign_up():
             flash('Passwor must be at least 7 characters.',category='error')
         else:
             #add user to database
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='256'))
             flash('Account created!',category='success')
     return render_template('sign_up.html')
