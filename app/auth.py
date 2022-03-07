253f7b21708564ccd062d635906e3996e2b9ffe6
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-07 12:37:50 +0300

    Import the db to the auth file and correct some naming mistakes to the first_name variables that were hindering creation of a new user account

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint,render_template,request,flash,redirect,url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth',__name__)
 
@@ -16,13 +17,13 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be over four characters.',category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('Name must be over 1 character.',category='error')
         elif password1 != password2:
             flash('Passwords do not match.',category='error')
@@ -30,7 +31,7 @@ def sign_up():
             flash('Passwor must be at least 7 characters.',category='error')
         else:
             #add user to database
-            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!',category='success')
--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class Pitch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(15000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 
