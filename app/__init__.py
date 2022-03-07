63fc349abf1b41b9fa59b7ec6eb866bdd538d0e7
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-07 10:17:42 +0300

    Initialize the database with the flask application

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ DB_NAME = 'database.db'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'nassehdenokinyuag'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
