28c4e4f01fcc1ca67b7c13592ba68e31e36f9fdd
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-07 10:55:48 +0300

    Add the Pitch Model to outline the structure of the pitch objects

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     first_name = db.Column(db.String(255))
+
+class Pitch(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(15000))
+    date = (db.Column(timezone=True), default=func.now())
+    
